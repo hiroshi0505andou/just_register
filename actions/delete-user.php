3d4844255778b58851bb584293f2df0d732da587
hiroshi0505andou <108224345+hiroshi0505andou@users.noreply.github.com>
2023-03-01 19:26:00 +0900

    Delete actions directory

--- actions/delete-user.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php
-
-include "../classes/User.php";
-
-$user_obj = new User;
-$user_obj->delete();
-
--- actions/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "../classes/User.php";
-
-//create object/instantiate
-$user_obj = new User;
-
-$user_obj->login($_POST);
-//$_POST contain all values of the input
